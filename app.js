da27e22c9dcf91d87311e6f9c51ad0865eeee5ac
BuketSenem <bouquet.exe@gmail.com>
2024-02-07 21:56:44 +0300

    finalize

--- app.js
+++ app.js
@@ -2,3 +2,17 @@ function toggleMenu() {
     var menu = document.querySelector('.menu');
     menu.classList.toggle('show');
 }
+document.addEventListener('DOMContentLoaded', function() {
+    var dropdown = document.querySelector('.dropdown');
+
+    dropdown.addEventListener('click', function() {
+        this.classList.toggle('clicked');
+    });
+
+    // Sayfa dışında bir yere tıklanırsa dropdown'ı kapat
+    document.addEventListener('click', function(event) {
+        if (!dropdown.contains(event.target)) {
+            dropdown.classList.remove('clicked');
+        }
+    });
+});
